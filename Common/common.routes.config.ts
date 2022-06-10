1f678adf212c05532ddc9eea837b533401f4f8a9
LeandroHurtado <leandro_david@hotmail.es>
2022-06-10 20:54:31 -0300

    feat: Common routes and user routes added

---
+++ Common/common.routes.config.ts
@@ -0,0 +1,12 @@
+import express from "express";
+export class CommonRoutesConfig {
+  app: express.Application;
+  name: String;
+  constructor(app: express.Application, name: String) {
+    this.app = app;
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+}
